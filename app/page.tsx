4284f770775cfc66e23175bb1241d3a6e8009a73
Melany MS <melamoralessoriano@gmail.com>
2024-04-26 22:43:17 -0600

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default async function Index() {
 
       <div className="flex flex-col items-center">
         <p>¿Te gustaría cambiar tu contraseña?</p>
-        <a href="http://localhost:3000/password" className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover mt-4">Actualizar</a>
+        <a href="/password" className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover mt-4">Actualizar</a>
       </div>
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
